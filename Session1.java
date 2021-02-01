f81028168677db3efb9fc6be9888c7ef1088c547
JumbleRuble <tanishasharda7@gmail.com>
2021-02-01 23:31:54 +0530

    Session 2 - loops

--- Session1.java
+++ Session1.java
@@ -37,6 +37,7 @@ class Session1{
 
         // if- else
 
+        /*
         int k = -7;
         if(k>0){
             System.out.println("Positive number");
@@ -62,6 +63,70 @@ class Session1{
         else
             System.out.println("Negative number");
 
+        */
+
+        // for loops 
+        // print 0-4
+        for (int i = 0; i < 5; i++) {
+            //System.out.println(i);
+        }
+
+        // odd numbers 0-10
+        for (int i = 1; i <= 10; i+=2) {
+            // System.out.println(i);
+        }
+
+        // print a string
+        //i=3
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i-1+") Tanisha");
+        }
+
+        // print 5-1
+        for(int i=5; i>=1; i--){
+            // System.out.println(i);
+        }
+
+        // brackets
+        for (int i = 0; i < 5; i++){ 
+            // System.out.println(i);
+            // System.out.println("Hi");
+        }
+
+        // int i=5;
+        // for( ; i>=1; i--){
+        //     // System.out.println(i);
+        // }
+
+        // System.out.println(i+"<-");
+
+        //-----------------------------------------------------
+        // while loop
+
+        int i = 10;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+
+        // comparison with for loop
+        for (int i = 10; i < 5; i++) {
+            System.out.println(i);
+        }
+
+
+
+        //--------------------------------------
+        // do while loop
+
+        // int i = 10;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<5);
 
     }
+
+    // nested for loops
 }
